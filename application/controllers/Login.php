d11ffe81bf21ebf88e1cffef156c91308f7cce30
Ricardo Orlando Castillo Olivera <ricardocastilloisc@gmail.com>
2021-01-24 13:11:20 -0500

    35. Creando nuestra vista para el login

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -18,18 +18,67 @@ class Login extends CI_Controller
 
 		//$hashPassword = hashPassword('12345');
 
-		echo verifyHashedPassword('12345', hashPassword('12345'));
+		//echo verifyHashedPassword('12345', hashPassword('12345'));
 
-		$this->load->view('user/login');
+		if(!$this->isLoggedIn())
+		{
+			$this->login();
+			$this->load->view('user/login');
+		
+		}else{
+
+			redirect('/core/dashboard');
+		}
+
 	}
 
 
 	private function isLoggedIn()
 	{
-		$user_id = $this->session->userdate('user_id');
+		$user_id = $this->session->userdata('user_id');;
 		if (isset($user_id)) {
 			return true;
 		}
 		return false;
+	}
+
+	private function login()
+	{
+		$this->load->helper(array('form', 'url'));
+
+		$this->load->library('form_validation');
+
+		$this->form_validation->set_rules('password', 'Password', 'required|max_length[20]');
+
+		$this->form_validation->set_rules('email', 'Email', 'required|max_length[30]|valid_email|trim');
+
+		if ($this->form_validation->run() == FALSE) {
+	
+		} else {
+			$email = $this->input->post('email');
+
+			$password = $this->input->post('password');
+
+			$user = $this->User->checkLogin($email,$password);
+
+			if(is_object($user))
+			{
+				$sessionUser = array(
+					'user_id' => $user->user_id,
+					'email' => $user->email,
+					'name' => $user->name
+				);
+
+				$this->session->set_userdata($sessionUser);
+				$this->session->set_flashdata("success", "Bienvenido $user->name");
+
+				redirect("/core/dashboard");
+			}else{
+
+				$this->session->set_flashdata("error", "Email o contraseña incorrecta $user->name");
+
+				echo "Email o contraseña incorrecta";
+			}
+		}
 	}
 }
--- application/views/user/login.php
+++ application/views/user/login.php
@@ -9,10 +9,10 @@
 <html>
 
 <head></head>
-	<title>My Awesome Login Page</title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+<title>My Awesome Login Page</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
 </head>
 <!--Coded with love by Mutiullah Samim-->
 
@@ -26,26 +26,26 @@
 					</div>
 				</div>
 				<div class="d-flex justify-content-center form_container">
-					<form>
+					<form method="POST" action="">
 						<div class="input-group mb-3">
 							<div class="input-group-append">
 								<span class="input-group-text"><i class="fas fa-user"></i></span>
 							</div>
-							<input type="text" name="" class="form-control input_user" value="" placeholder="username">
+							<input type="text" name="email" class="form-control input_user" value="" placeholder="username">
 						</div>
 						<div class="input-group mb-2">
 							<div class="input-group-append">
 								<span class="input-group-text"><i class="fas fa-key"></i></span>
 							</div>
-							<input type="password" name="" class="form-control input_pass" value="" placeholder="password">
+							<input type="password" name="password" class="form-control input_pass" value="" placeholder="password">
 						</div>
 
 						<div class="d-flex justify-content-center mt-3 login_container">
-							<button type="button" name="button" class="btn login_btn">Login</button>
+							<button type="submit" name="button" class="btn login_btn">Login</button>
 						</div>
 					</form>
 				</div>
-
+				<?php echo validation_errors(); ?>
 				<div class="mt-4">
 
 					<div class="d-flex justify-content-center links">
