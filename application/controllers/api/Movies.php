14a706c8832fdbcad5a1cd68b62248fa3c4ef984
Ricardo Orlando Castillo Olivera <ricardocastilloisc@gmail.com>
2021-01-23 23:34:38 -0500

    27. Búsqueda individual de una película

--- application/controllers/api/Movies.php
+++ application/controllers/api/Movies.php
@@ -6,31 +6,35 @@ use Restserver\Libraries\Rest_Controller;
 require APPPATH . '/libraries/Rest_Controller.php';
 require APPPATH . '/libraries/Format.php';
 
-class Movies extends Rest_Controller {
-	
+class Movies extends Rest_Controller
+{
 
-	public function show_get()
+
+	public function show_get($id = null)
 	{
-
-		//busqueda
-		$name = $this->input->get("name");
-
-		$search = explode(" ",$name);
-
-		//filtro
-		$type_movie_id = $this->input->get("type_movie_id");
-
-		$year = $this->input->get("year");
-		
-
-		//paginacion 
-		$offset = $this->input->get("offset");
-
-
 		$this->load->model('Movie');
-		
-		$data = $this->Movie->findRecords($search, $type_movie_id, $year, $offset);
 
+		if (isset($id)) {
+
+			$data = $this->Movie->find($id);
+	
+		} else {
+
+			//busqueda
+			$name = $this->input->get("name");
+
+			$search = explode(" ", $name);
+
+			//filtro
+			$type_movie_id = $this->input->get("type_movie_id");
+
+			$year = $this->input->get("year");
+
+			//paginacion 
+			$offset = $this->input->get("offset");
+
+			$data = $this->Movie->findRecords($search, $type_movie_id, $year, $offset);
+		}
 		$this->response($data);
 	}
 }
--- application/models/Movie.php
+++ application/models/Movie.php
@@ -1,29 +1,49 @@
 <?php
 
-class Movie extends MY_Model {
-	public function __construct(){
+class Movie extends MY_Model
+{
+	public function __construct()
+	{
 		parent::__construct();
 	}
 	public $table = "movies";
 	public $table_id = "movie_id";
 
-	function findall(){
+	function findall()
+	{
 		$this->db->select('m.*, tm.name as type_movie');
 		$this->db->from("$this->table as m");
 
-		$this->db->join('types_movie as tm','tm.type_movie_id = m.type_movie_id', 'LEFT');
+		$this->db->join('types_movie as tm', 'tm.type_movie_id = m.type_movie_id', 'LEFT');
 
 		$query  = $this->db->get();
 		return $query->result();
 	}
 
-	function findRecords($search = array(), $type_movie_id = null, $year = null, $offset = 0){
+	function find($id)
+	{
 
 		$this->db->select('m.*, tm.name as type_movie');
-		
+
 		$this->db->from("$this->table as m");
 
-		$this->db->join('types_movie as tm','tm.type_movie_id = m.type_movie_id', 'LEFT');
+		$this->db->join('types_movie as tm', 'tm.type_movie_id = m.type_movie_id', 'LEFT');
+
+		$this->db->where($this->table_id, $id);
+
+		$query  = $this->db->get();
+
+		return $query->row();
+	}
+
+	function findRecords($search = array(), $type_movie_id = null, $year = null, $offset = 0)
+	{
+
+		$this->db->select('m.*, tm.name as type_movie');
+
+		$this->db->from("$this->table as m");
+
+		$this->db->join('types_movie as tm', 'tm.type_movie_id = m.type_movie_id', 'LEFT');
 
 		//$busqueda
 
@@ -33,20 +53,18 @@ class Movie extends MY_Model {
 
 
 		//filtros
-		if(isset($type_movie_id)){
+		if (isset($type_movie_id)) {
 			$this->db->where('tm.type_movie_id', $type_movie_id);
 		}
 
-		if(isset($year)){
+		if (isset($year)) {
 			$this->db->where('year', $year);
 		}
 
-		$this->db->limit(PAGE_SIZE,$offset);
+		$this->db->limit(PAGE_SIZE, $offset);
 
 		$query  = $this->db->get();
 
 		return $query->result();
 	}
-
-
 }
