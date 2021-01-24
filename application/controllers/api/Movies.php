4e49ff0c5158dba1634ef3c2f1be4d47629a8dca
Ricardo Orlando Castillo Olivera <ricardocastilloisc@gmail.com>
2021-01-23 22:20:20 -0500

    Crear la api rest para las películas

---
+++ application/controllers/api/Movies.php
@@ -0,0 +1,21 @@
+<?php
+
+
+use Restserver\Libraries\Rest_Controller;
+
+require APPPATH . '/libraries/Rest_Controller.php';
+require APPPATH . '/libraries/Format.php';
+
+class Movies extends Rest_Controller {
+	
+
+	public function show_get()
+	{
+		$this->load->model('Movie');
+		
+		$data = $this->Movie->find(1);
+
+		$this->response($data);
+	}
+}
+/** End of file Controllername.php **/
